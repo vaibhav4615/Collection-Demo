563d0128c4eb92b0a1779c6c43ecd7187173db5c
vaibh <vaibh@DESKTOP-PHNAOI0>
2021-06-13 18:40:42 +0530

    Add project files.

---
+++ CollectionProject/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace CollectionProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            List<string> Emp = new();
+            Emp.Add("Da");
+            Emp.Add("Jane");
+            Emp.Add("Best");
+            Emp.Add("Friend");
+            Emp.Add("Home");
+            Emp.Add("Test");
+            Emp.Add("Data");
+            Emp.Add("Samrp");
+            Emp.Add("Garuj");
+            Emp.Add("paujn");
+
+            //Find one item
+            string ab = Emp.Find(f => f.Equals("Jane"));
+
+            //Find multiple Item
+            List<string> a = Emp.FindAll(f => f.Equals("Jane"));
+
+            Console.WriteLine(a);
+            Console.ReadLine();
+        }
+    }
+}
